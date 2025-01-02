aeb70d9172b984969137abcd48e160dfc55b68ab
mateuszpietrusinski <1325258@gmail.com>
2025-01-02 12:37:58 +0100

    fix: c3_2

--- AiSD5_C3/exercise_2/Pascal/main.pas
+++ AiSD5_C3/exercise_2/Pascal/main.pas
@@ -3,15 +3,21 @@ program SilniaPomiarCzasu;
 uses CRT, math, sysUtils;
 
 { Deklaracje wyprzedzające funkcji i procedur }
-function WczytajLiczbe(komunikat: string; minWartosc, maxWartosc: Int64): Int64; forward;
+function WczytajLiczbe(const komunikat: string; minWartosc: Int64; maxWartosc: Int64): Int64; forward;
 function SilniaRekurencyjna(n: Int64): Int64; forward;
 function SilniaIteracyjna(n: Int64): Int64; forward;
-procedure WykonajPomiaryCzasu(n, liczbaPowtorzen: Int64); forward;
-procedure WyswietlWyniki(n, liczbaPowtorzen: Int64; wartoscSilni: Int64; czasIteracyjny, czasRekurencyjny: Double); forward;
+procedure WykonajPomiaryCzasu(n: Int64; liczbaPowtorzen: Int64); forward;
+procedure WyswietlWyniki(n: Int64; liczbaPowtorzen: Int64; wartoscSilni: Int64; czasIteracyjny: Double; czasRekurencyjny: Double); forward;
 procedure Kontroler; forward;
 
+{ Funkcja sprawdzająca, czy wynik nie przekroczy zakresu Int64 }
+function CzyMoznaObliczyc(n: Int64): Boolean;
+begin
+    CzyMoznaObliczyc := n <= 20; { Empirycznie ustalone maksimum dla Int64 }
+end;
+
 { Funkcja do wczytywania i walidacji liczby wejściowej }
-function WczytajLiczbe(komunikat: string; minWartosc, maxWartosc: Int64): Int64;
+function WczytajLiczbe(const komunikat: string; minWartosc: Int64; maxWartosc: Int64): Int64;
 var
     n: Int64;
     kod: Integer;
@@ -30,55 +36,68 @@ begin
     WczytajLiczbe := n;
 end;
 
-{ Obliczanie silni metodą rekurencyjną }
-function SilniaRekurencyjna(n: Int64): Int64;
-begin
-    if n = 0 then
-        SilniaRekurencyjna := 1
-    else
-        SilniaRekurencyjna := n * SilniaRekurencyjna(n - 1);
-end;
-
 { Obliczanie silni metodą iteracyjną }
 function SilniaIteracyjna(n: Int64): Int64;
 var
     wynik: Int64;
     i: Int64;
 begin
+    if not CzyMoznaObliczyc(n) then
+    begin
+        WriteLn('Błąd: Wartość zbyt duża dla typu Int64');
+        Halt(1);
+    end;
+    
     wynik := 1;
-    for i := 1 to n do
+    for i := 2 to n do
         wynik := wynik * i;
     SilniaIteracyjna := wynik;
 end;
 
+{ Obliczanie silni metodą rekurencyjną }
+function SilniaRekurencyjna(n: Int64): Int64;
+begin
+    if not CzyMoznaObliczyc(n) then
+    begin
+        WriteLn('Błąd: Wartość zbyt duża dla typu Int64');
+        Halt(1);
+    end;
+    
+    if n <= 1 then
+        SilniaRekurencyjna := 1
+    else
+        SilniaRekurencyjna := n * SilniaRekurencyjna(n - 1);
+end;
+
 { Procedura wykonująca pomiary czasu }
-procedure WykonajPomiaryCzasu(n, liczbaPowtorzen: Int64);
+procedure WykonajPomiaryCzasu(n: Int64; liczbaPowtorzen: Int64);
 var
     i: Int64;
-    czasStartowy, czasKoncowy: Int64;
+    czasStartowy, czasKoncowy: QWord;
     wartoscSilni: Int64;
     czasIteracyjny, czasRekurencyjny: Double;
 begin
     { Pomiar czasu dla metody iteracyjnej }
     czasStartowy := GetTickCount64;
-    for i := 1 to liczbaPowtorzen do
-        wartoscSilni := SilniaIteracyjna(n);
+    wartoscSilni := SilniaIteracyjna(n);
+    for i := 2 to liczbaPowtorzen do
+        SilniaIteracyjna(n);
     czasKoncowy := GetTickCount64;
-    czasIteracyjny := (czasKoncowy - czasStartowy) / 1000.0; // Konwersja na sekundy
+    czasIteracyjny := (czasKoncowy - czasStartowy) / 1000.0;
 
     { Pomiar czasu dla metody rekurencyjnej }
     czasStartowy := GetTickCount64;
     for i := 1 to liczbaPowtorzen do
-        wartoscSilni := SilniaRekurencyjna(n);
+        SilniaRekurencyjna(n);
     czasKoncowy := GetTickCount64;
-    czasRekurencyjny := (czasKoncowy - czasStartowy) / 1000.0; // Konwersja na sekundy
+    czasRekurencyjny := (czasKoncowy - czasStartowy) / 1000.0;
 
     { Wyświetl wyniki }
     WyswietlWyniki(n, liczbaPowtorzen, wartoscSilni, czasIteracyjny, czasRekurencyjny);
 end;
 
 { Wyświetlanie wyników z odpowiednim formatowaniem }
-procedure WyswietlWyniki(n, liczbaPowtorzen: Int64; wartoscSilni: Int64; czasIteracyjny, czasRekurencyjny: Double);
+procedure WyswietlWyniki(n: Int64; liczbaPowtorzen: Int64; wartoscSilni: Int64; czasIteracyjny: Double; czasRekurencyjny: Double);
 begin
     WriteLn;
     WriteLn('Wyniki pomiarów:');
@@ -92,8 +111,10 @@ begin
     
     if czasRekurencyjny > czasIteracyjny then
         WriteLn('Metoda rekurencyjna jest wolniejsza o ', (czasRekurencyjny/czasIteracyjny):0:2, ' razy')
+    else if czasRekurencyjny < czasIteracyjny then
+        WriteLn('Metoda iteracyjna jest wolniejsza o ', (czasIteracyjny/czasRekurencyjny):0:2, ' razy')
     else
-        WriteLn('Metoda iteracyjna jest wolniejsza o ', (czasIteracyjny/czasRekurencyjny):0:2, ' razy');
+        WriteLn('Obie metody mają podobny czas wykonania');
 end;
 
 { Procedura sterująca }
@@ -102,18 +123,16 @@ var
     n, liczbaPowtorzen: Int64;
     maxDozwolonyArgument: Int64;
 begin
-    { Ustaw maksymalną dozwoloną wartość argumentu }
-    maxDozwolonyArgument := 20; { Dla Int64 - ustalono empirycznie }
+    maxDozwolonyArgument := 20;  { Maksymalna bezpieczna wartość dla Int64 }
     
-    { Wczytaj parametry }
     n := WczytajLiczbe('Podaj argument silni (0-' + IntToStr(maxDozwolonyArgument) + '): ', 0, maxDozwolonyArgument);
-    liczbaPowtorzen := WczytajLiczbe('Podaj liczbę powtórzeń (min. 1): ', 1, High(Int64));
+    liczbaPowtorzen := WczytajLiczbe('Podaj liczbę powtórzeń (min. 1): ', 1, High(Int64));  { Bez górnego limitu }
     
-    { Wykonaj pomiary }
     WykonajPomiaryCzasu(n, liczbaPowtorzen);
 end;
 
-{ Program główny }
 begin
     Kontroler;
+    Write('Naciśnij Enter, aby zakończyć...');
+    ReadLn;
 end.
